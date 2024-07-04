fd3cd6a0b8c37a8007982ec6ab929735cbcee52d
alma8026 <alikswtf@gmail.com>
2024-07-04 16:02:29 +0200

    starting numpy

--- Python-ejs/libraries/pandas/learn-pandas.py
+++ Python-ejs/libraries/pandas/learn-pandas.py
@@ -11,3 +11,30 @@ print("")
 print("Columnas:")
 print(df.columns)
 # %%
+import pandas as pd
+
+df = pd.read_csv('../../../../../vehicles.csv') # todas las filas y cols del CSV original -> 1.45Gb
+
+# Crear una copia del DataFrame con las 10 primeras filas
+df_copia = df.head(10).copy()
+
+# Guardar en otro CSV:
+# mechatroner.rainbow-csv CSV Rainbow para visualizar columna por colores
+df_copia.to_csv("vehicles_acortado.csv", index=False) 
+
+# %%
+import pandas as pd
+
+# Crear un DataFrame de ejemplo
+data = {
+    'Nombre': ['Juan', 'Ana', 'Luis', 'Maria', 'Juan'],
+    'Edad': [20, 24, 22, 32, 30],
+    'Ciudad': ['Madrid', 'Barcelona', 'Valencia', 'Sevilla', 'Madrid']
+}
+df = pd.DataFrame(data)
+
+# Agrupar por la columna 'Ciudad' y calcular la edad promedio:
+df_agrupado = df.groupby('Ciudad')['Edad'].mean()
+
+print(df_agrupado)
+# %%
